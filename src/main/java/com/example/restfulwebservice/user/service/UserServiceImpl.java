68f66664f82f9061d99dcfa784ce5fecf5e77555
LEEJ0NGWAN <lastffx@gmail.com>
2021-04-21 20:32:24 +0900

    user service

---
+++ src/main/java/com/example/restfulwebservice/user/service/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.restfulwebservice.user.service;
+
+import com.example.restfulwebservice.user.User;
+import com.example.restfulwebservice.user.dao.UserDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private UserDAO userDAO;
+
+    @Autowired
+    public UserServiceImpl(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userDAO.selectAll();
+    }
+
+    @Override
+    public User find(Long id) {
+        return userDAO.select(id);
+    }
+
+    @Override
+    public User save(User user) {
+        return userDAO.insert(user);
+    }
+}
