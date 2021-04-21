211696698515bfdb22c1d1534cb89b584dfe8dc6
LEEJ0NGWAN <lastffx@gmail.com>
2021-04-21 19:56:39 +0900

    path variable 추가

--- src/main/java/com/example/restfulwebservice/hello/HelloController.java
+++ src/main/java/com/example/restfulwebservice/hello/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.restfulwebservice.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,17 +11,27 @@ public class HelloController {
      * [GET] hello
      */
     // RequestMapping(mehod=RequestMethod.GET, path="/hello")
-    @GetMapping(path="/hello")
+    @GetMapping(path = "/hello")
     public String hello() {
         return "hello";
     }
 
     /**
      * [GET] hello-bean
+     *
      * @return
      */
-    @GetMapping(path="/hello-bean")
+    @GetMapping(path = "/hello-bean")
     public HelloBean helloBean() {
         return new HelloBean("hihi");
     }
+
+    /**
+     * [GET] hello-path-var
+     *
+     * @param var
+     * @return
+     */
+    @GetMapping(path = "/hello-path-var/{var}")
+    public HelloBean helloBean(@PathVariable String var) { return  new HelloBean(var); }
 }
