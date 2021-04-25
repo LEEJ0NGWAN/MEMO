363c2d75090d24ea66e6891bbdcaff3d40c4b705
LEEJ0NGWAN <lastffx@gmail.com>
2021-04-25 17:33:16 +0900

    user model validation 옵션 추가

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -6,12 +6,19 @@ import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
 
     private Long id;
+
+    @Size(min=2, message = "2글자 이상으로 입력하세요")
     private String name;
+
+    @Past
     private Date joinDate;
 }
