98bf38a34ef3c000629012cd64277f3f9a05872e
LEEJ0NGWAN <lastffx@gmail.com>
2021-04-25 17:34:17 +0900

    @valid 유효성에 대한 예외 처리

--- src/main/java/com/example/restfulwebservice/exception/MyResponseEntityExceptionHandler.java
+++ src/main/java/com/example/restfulwebservice/exception/MyResponseEntityExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.example.restfulwebservice.exception;
 
 import com.example.restfulwebservice.user.exception.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,16 +18,25 @@ import java.util.Date;
 public class MyResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public final ResponseEntity<Object> allExceptions(Exception exception, WebRequest request) {
+    public final ResponseEntity<Object> allExceptions(Exception ex, WebRequest request) {
         return new ResponseEntity(
-                new ExceptionResponse(new Date(), exception.getMessage(), request.getDescription(false)),
+                new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false)),
                 HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    public final ResponseEntity<Object> userNotFoundException(Exception exception, WebRequest request) {
+    public final ResponseEntity<Object> userNotFoundException(Exception ex, WebRequest request) {
         return new ResponseEntity(
-                new ExceptionResponse(new Date(), exception.getMessage(), request.getDescription(false)),
+                new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false)),
                 HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        return new ResponseEntity(
+                new ExceptionResponse(new Date(), "Validation Error", ex.getBindingResult().toString()),
+                HttpStatus.BAD_REQUEST);
     }
 }
