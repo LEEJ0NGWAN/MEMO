cd017606830bbd2085cdb4b46d24007d6c58a29d
LEEJ0NGWAN <lastffx@gmail.com>
2021-05-02 17:06:00 +0900

    versioning api

--- src/main/java/com/example/restfulwebservice/user/AdminUserController.java
+++ src/main/java/com/example/restfulwebservice/user/AdminUserController.java
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -45,8 +47,10 @@ public class AdminUserController {
         return ResponseEntity.ok(mappingJacksonValue);
     }
 
-    @GetMapping(path = "/users/{id}")
-    public ResponseEntity<MappingJacksonValue> retrieveUser(@PathVariable Long id) {
+//    @GetMapping(path = "/users/{id}", params = "version=1")
+//    @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=1")
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv1+json")
+    public ResponseEntity<MappingJacksonValue> retrieveUserV1(@PathVariable Long id) {
         User user = userService.find(id);
         if (user == null)
             throw new UserNotFoundException(String.format("id=%d not found",id));
@@ -57,6 +61,29 @@ public class AdminUserController {
         FilterProvider filterProvider = new SimpleFilterProvider().addFilter("UserInfo", simpleBeanPropertyFilter);
 
         MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(user);
+        mappingJacksonValue.setFilters(filterProvider);
+
+        return ResponseEntity.ok(mappingJacksonValue);
+    }
+
+//    @GetMapping(path = "/users/{id}/", params = "version=2")
+//    @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=2")
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv2+json")
+    public ResponseEntity<MappingJacksonValue> retrieveUserV2(@PathVariable Long id) {
+        User user = userService.find(id);
+        if (user == null)
+            throw new UserNotFoundException(String.format("id=%d not found",id));
+
+        UserV2 userV2 = new UserV2();
+        BeanUtils.copyProperties(user,userV2);
+        userV2.setGrade("VIP");
+
+        SimpleBeanPropertyFilter simpleBeanPropertyFilter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id","name","joinDate","password","ssn","grade");
+
+        FilterProvider filterProvider = new SimpleFilterProvider().addFilter("UserInfoV2", simpleBeanPropertyFilter);
+
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(userV2);
         mappingJacksonValue.setFilters(filterProvider);
 
         return ResponseEntity.ok(mappingJacksonValue);
