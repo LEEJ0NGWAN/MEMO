0f6c4f67e28af4873b37ea16cbd1db9fef9eaba9
LEEJ0NGWAN <lastffx@gmail.com>
2021-05-10 00:49:16 +0900

    swagger documentation info

--- src/main/java/com/example/restfulwebservice/config/SwaggerConfig.java
+++ src/main/java/com/example/restfulwebservice/config/SwaggerConfig.java
@@ -2,19 +2,46 @@ package com.example.restfulwebservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
+    private static final Contact DEFAULT_CONTACT =
+            new Contact("이종완","test.com","ljw@test.com");
+
+    private static final ApiInfo DEFAULT_API_INFO =
+            new ApiInfoBuilder()
+            .title("유저 관리 api")
+            .description("유저를 관리하는 기능을 제공하는 api 입니다.")
+            .version("1.0")
+            .termsOfServiceUrl("urn:tos")
+            .contact(DEFAULT_CONTACT)
+            .license("Apache 2.0")
+            .licenseUrl("http://www.apache.org/license/LICENSE-2.0")
+            .build();
+
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES =
+            new HashSet<>(Arrays.asList("application/json", "application/xml"));
+
     @Bean
     public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
