9f7f514c3f90bb28709a8f3136fd06f639037072
LEEJ0NGWAN <lastffx@gmail.com>
2021-05-19 00:00:50 +0900

    test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,8 @@
 insert into user values (101,sysdate(),'a','a','a')
 insert into user values (102,sysdate(),'b','b','b')
 insert into user values (103,sysdate(),'c','c','c')
+
+insert into post values (1001,"post 1",101);
+insert into post values (1002,"post 2",101);
+insert into post values (1003,"post 3",102);
+insert into post values (1004,"post 4",103);
