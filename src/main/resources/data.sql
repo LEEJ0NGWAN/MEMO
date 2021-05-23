08ce6900de955e31270f61ca1cb12683d2d45bba
LEEJ0NGWAN <lastffx@gmail.com>
2021-05-23 23:10:17 +0900

    test: dummy post data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,11 @@ insert into user values (101,sysdate(),'a','a','a')
 insert into user values (102,sysdate(),'b','b','b')
 insert into user values (103,sysdate(),'c','c','c')
 
-insert into post values (1001,"post 1",101);
-insert into post values (1002,"post 2",101);
-insert into post values (1003,"post 3",102);
-insert into post values (1004,"post 4",103);
+insert into post values (1001,'post 1',101)
+insert into post values (1002,'post 2',101)
+insert into post values (1003,'post 3',102)
+insert into post values (1004,'post 4',103)
+insert into post values (1005,'post 5',103)
+insert into post values (1006,'post 6',103)
+insert into post values (1007,'post 7',103)
+insert into post values (1008,'post 8',103)
